c97f77e9bed1f4926665c5cbd855a57b82fa4b92
Micherlane Silva <micherlanerod@gmail.com>
2023-10-21 09:52:46 -0300

    Configuração Swagger

---
+++ GetDog/backend/config/swagger/swaggerConfig.ts
@@ -0,0 +1,9 @@
+import { DocumentBuilder } from "@nestjs/swagger";
+
+export const swaggerConfig = new DocumentBuilder()
+            .setTitle('Documentação API rede social GetDog')
+            .setDescription('A presente documentação é um guia de como poder acessar os recursos da rede social GetDog')
+            .setVersion('1.0')
+            .addTag('Usuario')
+            .addTag('Postagem')
+            .build();
--- GetDog/backend/src/main.ts
+++ GetDog/backend/src/main.ts
@@ -3,11 +3,16 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { VersioningType } from '@nestjs/common';
 import { join } from 'path';
+import { SwaggerModule } from '@nestjs/swagger';
+import { swaggerConfig } from 'config/swagger/swaggerConfig';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true
   });
+
+  const swaggerDocument =  SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swaggerDocument);
 
   app.enableVersioning({
     type: VersioningType.URI,
