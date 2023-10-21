4567b6f20d1e7371b01570f3879e47972ea4443d
Micherlane Silva <micherlanerod@gmail.com>
2023-10-21 09:42:09 -0300

    Habilitando cors e statics

--- GetDog/backend/src/main.ts
+++ GetDog/backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { VersioningType } from '@nestjs/common';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true
+  });
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  })
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
   await app.listen(3000);
 }
 bootstrap();
