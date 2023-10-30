bf2bda1aa5052f425bd48fdd358c5066261cd773
SoLaMente Dev <testercod3@gmail.com>
2023-10-30 12:25:21 -0300

    Teste 400 /v1/users (POST)

---
+++ GetDog/backend/test/user.create.e2e-spec.ts
@@ -0,0 +1,33 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+
+describe('AppController (e2e)', ()=>{
+    let app: INestApplication;
+
+    beforeAll(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule]
+        }).compile();
+
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
+
+    afterAll(async () => {
+        await app.close();
+    });
+
+    it('/v1/users (POST)', () => {
+        return request(app.getHttpServer())
+            .post('/v1/users')
+            .send({
+                name: "Teste",
+                password: "teste",
+                address: "Rua Teste, Nº Teste",
+                typeUser: "DOG_OWNER"
+            })
+            .expect(400)           
+    });
+})
