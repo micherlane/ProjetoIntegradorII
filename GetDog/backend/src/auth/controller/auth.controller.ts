4219a586dfa02b9fb3f0d009996e92cf452835a1
Micherlane Silva <micherlanerod@gmail.com>
2023-10-31 18:00:07 -0300

    Validando entrada de dados autenticação.

--- GetDog/backend/src/auth/controller/auth.controller.ts
+++ GetDog/backend/src/auth/controller/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
 import { AuthUserService } from '../service/auth.service';
 import { AuthDto } from '../dto/auth.login.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { ExceptionError } from 'src/middlewares/exceptions/exception.error';
 
 @ApiTags("Usuario")
 @Controller({
@@ -12,6 +13,9 @@ export class AuthUserController {
 
     @Post("/session")
     async signIn(@Res() res, @Body() authLoginDto: AuthDto){
+        if(!authLoginDto.password || !authLoginDto.password){
+            throw new ExceptionError("Forneça o email/senha", HttpStatus.BAD_REQUEST);
+        }
         const auth = await this.authService.execute(authLoginDto);
 
         return res.status(HttpStatus.OK).json(auth);
