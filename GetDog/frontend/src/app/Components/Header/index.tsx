61105cdb43751671f113eaaa57dde3e3a2f149ca
Micherlane Silva <micherlanerod@gmail.com>
2023-11-13 17:19:49 -0300

    Header Concluído

---
+++ GetDog/frontend/src/app/Components/Header/index.tsx
@@ -0,0 +1,37 @@
+import styles from './styles.module.css';
+import { FiLogOut } from 'react-icons/fi';
+
+export function Header(){
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+
+                <div className={styles.headerLogo}>
+                    <p>GetDog</p>
+                </div>
+
+                <div className={styles.headerUserInfo}>
+
+                    <div className={styles.headerUser}>
+
+                        <div className={styles.headerUserImage}>
+                            <img src='http://localhost:3001/images/1699906205195.ai_image_history_62844.png'/>
+                        </div>
+
+                        <div className={styles.headerUserName}>
+                            <p>Nome do Usuário</p>
+                        </div>
+
+                        <div className={styles.headerButtonLogOut}>
+
+                            <button>
+                                <FiLogOut color='#000' size={24}></FiLogOut>
+                            </button>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+}
