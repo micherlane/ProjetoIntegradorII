bbc1cff3d1657b14798a4025b7a855b6f659d23c
Micherlane Silva <micherlanerod@gmail.com>
2023-11-15 10:33:52 -0300

    Componente de adicionar postagem.

--- GetDog/frontend/src/app/HomePage/components/PostAdd/index.tsx
+++ GetDog/frontend/src/app/HomePage/components/PostAdd/index.tsx
@@ -1,11 +1,18 @@
+'use client';
+
 import { ImageUser } from '@/app/Components/ImageUser';
 import styles from './styles.module.css';
+import { toast } from 'react-toastify';
 
 export function PostAdd(){
+    const handlePostClick = () => {
+       toast.success("Você clicou em adicionar postagem") ;
+    };
+
     return (
         <div className={styles.postAddContainer}>
             <ImageUser urlImage='http://localhost:3001/images/1699906205195.ai_image_history_62844.png' size={50}/>
-            <div className={styles.postWritterContainer}>
+            <div className={styles.postWritterContainer} onClick={handlePostClick}>
                 <p>Escreva sua publicação...</p>
             </div>
         </div>
