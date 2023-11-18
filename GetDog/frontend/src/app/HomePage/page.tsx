876be73177eb578e7f0fce6ea67534ccef795ac6
Micherlane Silva <micherlanerod@gmail.com>
2023-11-18 09:35:38 -0300

    Alterado o título da página.

--- GetDog/frontend/src/app/HomePage/page.tsx
+++ GetDog/frontend/src/app/HomePage/page.tsx
@@ -1,12 +1,21 @@
+import { Metadata } from "next";
 import { Header } from "../Components/Header";
 import { PostAdd } from "./components/PostAdd";
 import { PostItem } from "./components/PostItem";
 
+export const metadata: Metadata = {
+    title: 'Home',
+}
+
+  
 export default function HomePage(){
     return (
         <>
         <Header/>
         <PostAdd/>
+        <PostItem/>
+        <PostItem/>
+        <PostItem/>
         <PostItem/>
         </>
     );
