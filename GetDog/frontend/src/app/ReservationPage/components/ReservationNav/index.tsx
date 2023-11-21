0b4afd35e0356f29557644f953cf835687248507
Micherlane Silva <micherlanerod@gmail.com>
2023-11-20 21:49:08 -0300

    Rota de reservas.

---
+++ GetDog/frontend/src/app/ReservationPage/components/ReservationNav/index.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+import styles from './styles.module.css';
+import { useState } from 'react';
+import { IoMenu } from "react-icons/io5";
+
+export function ReservationNav(){
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    }
+
+    const hideMenu = () => {
+        setShowMenu(false);
+    };
+
+    return (
+        <div className={styles.reservationNavContainer}>
+            <IoMenu className={styles.toggleButton} onClick={toggleMenu} color="#ffffff" size={45}/>
+            <div className={`${styles.reservationNavStyle} ${showMenu ? styles.showMenu : ''}`}>
+                <Link href="#" onClick={hideMenu}>
+                    MINHAS RESERVAS
+                </Link>
+                <Link href="#" onClick={hideMenu}>
+                    RESERVAS SOLICITADAS
+                </Link>
+            </div>
+        </div>
+    );
+}
---
+++ GetDog/frontend/src/app/ReservationPage/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Header } from "../Components/Header";
+import { ReservationNav } from "./components/ReservationNav";
+
+export default function ReservationPage(){
+    return(
+        <>
+            <Header/>
+            <ReservationNav/>
+        </>
+    );
+}
