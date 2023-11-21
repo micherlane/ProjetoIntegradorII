0a8f9654e57c57a27149c17a3bea87471084c9ae
Micherlane Silva <micherlanerod@gmail.com>
2023-11-20 21:57:42 -0300

    Menu toggle finalizado.

--- GetDog/frontend/src/app/ReservationPage/components/ReservationNav/index.tsx
+++ GetDog/frontend/src/app/ReservationPage/components/ReservationNav/index.tsx
@@ -16,7 +16,7 @@ export function ReservationNav(){
 
     return (
         <div className={styles.reservationNavContainer}>
-            <IoMenu className={styles.toggleButton} onClick={toggleMenu} color="#ffffff" size={45}/>
+            <IoMenu className={styles.toggleButton} onClick={toggleMenu} color="#ffffff" size={70}/>
             <div className={`${styles.reservationNavStyle} ${showMenu ? styles.showMenu : ''}`}>
                 <Link href="#" onClick={hideMenu}>
                     MINHAS RESERVAS
