1a9eedf4005fd5fefa6db50199d315264eed7b5f
Micherlane Silva <micherlanerod@gmail.com>
2023-11-22 20:47:31 -0300

    Tela de Login Finalizada.

--- GetDog/frontend/src/app/page.tsx
+++ GetDog/frontend/src/app/page.tsx
@@ -1,22 +1,21 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <>
       <main className={styles.main}>
-          <div className={styles.description}>
-            <p>GetDog</p>
-            <p>GetDog é uma rede social destinada às pessoas que desejam divulgar seus serviços de Dogwalks e aos amantes de cachorros que precisam de alguém para passear com seus caẽs.</p>
+          <div className={styles.descriptionSession}>
+            <p className={styles.titleGetDog}>GetDog</p>
+            <p className={styles.description}>GetDog é uma rede social destinada às pessoas que desejam divulgar seus serviços de Dogwalks e aos amantes de cachorros que precisam de alguém para passear com seus caẽs.</p>
           </div>
           <div className={styles.login}>
               <form>
-                <input type='text' placeholder='Digite seu email...'/>
-                <input type='password' placeholder='Digite sua senha...'/>
-                <button type='submit'>Entrar</button>
+                <input type='text' placeholder='Email'/>
+                <input type='password' placeholder='Senha'/>
+                <button type='submit' className={styles.buttonEnter}>Entrar</button>
               </form>
 
-              <button>Criar nova conta</button>
+              <button className={styles.buttonNewAccount}>Criar nova conta</button>
           </div>
       </main>
     </>
