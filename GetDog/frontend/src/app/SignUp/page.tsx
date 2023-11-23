c375c2124761c591a26ca1b47773c563516828e4
SoLaMente Dev <testercod3@gmail.com>
2023-11-23 09:02:47 -0300

    Tela de signUp concluída

---
+++ GetDog/frontend/src/app/SignUp/page.tsx
@@ -0,0 +1,25 @@
+import styles from './style.module.css';
+
+export default function SignUp(){
+    return (
+        <>
+            <main className={styles.main}>
+                <h1 className={styles.titleGetDog}>GetDog</h1>
+                <div className={styles.signUp}>
+                    <h2>Registre-se</h2>
+                    <form>
+                        <input type='text' placeholder='Nome'/>
+                        <input type='text' placeholder='Email'/>
+                        <input type='text' placeholder='Endereço'/>
+                        <input type='password' placeholder='Senha'/>
+                        <select name="typeUser">
+                            <option value="DOG_OWNER">Tutor</option>
+                            <option value="DOG_WALK">Passeador</option>
+                        </select>
+                        <button type='submit' className={styles.register}>Registrar</button>
+                    </form>
+                </div>
+            </main>
+        </>
+    )
+}
