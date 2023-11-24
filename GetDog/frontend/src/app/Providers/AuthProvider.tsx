8c5dec7a52d4175d6c30efa3942b6d97079b1464
SoLaMente Dev <testercod3@gmail.com>
2023-11-24 11:17:45 -0300

    Lógica inicial do authProvider

--- GetDog/frontend/src/app/Providers/AuthProvider.tsx
+++ GetDog/frontend/src/app/Providers/AuthProvider.tsx
@@ -3,29 +3,55 @@
 import { ReactNode, useState } from "react";
 import { AuthContext, SignInProps, UserProps } from "../contexts/AuthContext";
 
-import { destroyCookie } from 'nookies';
+import { destroyCookie, setCookie, parseCookies } from 'nookies';
 
 import Router from 'next/router';
+
+import { api } from "../services/apiClient";
 
 type AuthProviderProps = {
     children: ReactNode;
 }
 
+
 export function signOut() {
     try {
         destroyCookie(undefined, '@getdog.token');
-        Router.push('/');
-    } catch {
-        console.log('Erro ao deslogar');
+    } catch (err){
+        console.log('Erro ao deslogar', err);
     }
 }
-
 export function AuthProvider ({ children} : AuthProviderProps) {
     const [user, setUser] = useState<UserProps>();
     const isAuthenticated = !!user;
     
     async function signIn({email, password}: SignInProps){
-        console.log(email, password);
+        try {
+            const response = await api.post('/session', {
+                email,
+                password
+            });
+
+            const { id, name, address, typeUser, token} = response.data;
+            setCookie(undefined, '@getdog.token', token, {
+                maxAge: 60 * 60 * 24 * 30,
+                path: "/"
+            });
+
+            setUser({
+                id, 
+                name,
+                email,
+                typeUser,
+                address
+            });
+
+            api.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+
+        } catch (err){
+            console.log('Erro ao acessar', err);
+        }
     }
 
     return (
