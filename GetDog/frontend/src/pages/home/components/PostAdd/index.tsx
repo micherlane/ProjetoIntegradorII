0754982aab072abbe4ded62cac265051100d037b
Micherlane Silva <micherlanerod@gmail.com>
2023-11-26 11:11:16 -0300

    Atualizando imagem em adicionar postagem.

--- GetDog/frontend/src/pages/home/components/PostAdd/index.tsx
+++ GetDog/frontend/src/pages/home/components/PostAdd/index.tsx
@@ -3,15 +3,26 @@
 import styles from './styles.module.css';
 import { toast } from 'react-toastify';
 import Modal from 'react-modal';
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { PostModel } from '@/models/postModel';
 import { ImageUser } from '@/components/ImageUser';
 import { ModalPost } from '@/components/ModalPost';
+import { AuthContext } from '@/contexts/AuthContext';
 
 interface PostAddProps{
     handleAddPost: (post: PostModel) => void;
 }
 export function PostAdd({handleAddPost}: PostAddProps){
+    const { user } = useContext(AuthContext);
+    const [urlUser, setUrlUser] = useState('');
+
+    useEffect(() => {
+        if (user) {
+            const url = `http://localhost:3001/images/${user.profile.profilePicture}`;
+            setUrlUser(url);
+        }
+    }, [user]);
+    
     const [modalVisible, setModalVisible] = useState(false);
 
     const handlePostClick = () => {
@@ -30,7 +41,7 @@ export function PostAdd({handleAddPost}: PostAddProps){
     return (
         <div>
             <div className={styles.postAddContainer}>
-                <ImageUser urlImage='http://localhost:3001/images/1699906205195.ai_image_history_62844.png' size={50}/>
+                <ImageUser urlImage={urlUser} size={50}/>
                 <div className={styles.postWritterContainer} onClick={handlePostClick}>
                     <p>Escreva sua publicação...</p>
                 </div>
