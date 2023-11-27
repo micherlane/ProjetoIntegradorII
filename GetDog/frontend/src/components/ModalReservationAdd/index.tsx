8679b7bef750cf83bb4f47732084fb314c404e07
Micherlane Silva <micherlanerod@gmail.com>
2023-11-27 19:04:29 -0300

    Modal para criar reserva.

--- GetDog/frontend/src/components/ModalReservationAdd/index.tsx
+++ GetDog/frontend/src/components/ModalReservationAdd/index.tsx
@@ -1,8 +1,10 @@
-import styles from './styles.module.css';
 import Modal from 'react-modal';
-
+import styles from './styles.module.css';
+import { useContext, useState } from 'react';
+import { AuthContext } from '@/contexts/AuthContext';
 import { FiX } from 'react-icons/fi';
 import { PostModel } from '@/models/postModel';
+import { DisponibilityComponent } from '../DisponibilityComponent';
 
 interface ModalReservationAddProps {
     isOpen: boolean;
@@ -10,7 +12,30 @@ interface ModalReservationAddProps {
     post: PostModel;
 }
 
-export function ModalReservationAdd({isOpen, onRequestClose, post}: ModalReservationAddProps){
+export function ModalReservationAdd({ isOpen, onRequestClose, post }: ModalReservationAddProps) {
+    const { user } = useContext(AuthContext);
+
+    const [disponibilityStatus, setDisponibilityStatus] = useState(Array(post.disponiblity.length).fill(false));
+
+    const selectDisponibility = (index: number) => {
+        let disponibilitysStatusUpdated = [...disponibilityStatus];
+
+        disponibilitysStatusUpdated = disponibilityStatus.map((disponibility, i) => {
+            if (index === i) {
+                disponibility = !disponibility;
+                return disponibility;
+            }
+
+            disponibility = false;
+
+            return disponibility;
+        });
+
+        setDisponibilityStatus(disponibilitysStatusUpdated);
+
+        console.log(disponibilitysStatusUpdated)
+
+    }
 
     const customStyle = {
         overlay: {
@@ -28,10 +53,10 @@ export function ModalReservationAdd({isOpen, onRequestClose, post}: ModalReserva
         }
     }
 
-    const handleAdd = () => {}
+    const handleAdd = () => { }
 
     return (
-        <Modal 
+        <Modal
             isOpen={isOpen}
             onRequestClose={onRequestClose}
             style={customStyle}
@@ -44,6 +69,40 @@ export function ModalReservationAdd({isOpen, onRequestClose, post}: ModalReserva
             >
                 <FiX size={30} color="#ff0000" />
             </button>
+
+            <div className={styles.formStyle}>
+                <form>
+                    <div className={styles.titleReservationContainer}>
+                        <h2 className={styles.titleReservation}>Reservar Passeio</h2>
+                    </div>
+
+                    <div className={styles.postContainer}>
+                        <p className={styles.titlePost}>{post.title}</p>
+                        <p className={styles.authorPost}>{post.author.name}</p>
+                        <p className={styles.addressPost}>{post.address}</p>
+                    </div>
+                    <div className={styles.disponibilityContainer}>
+                        <p>Escolha uma data</p>
+                        <div className={styles.disponibility}>
+                            {
+                                post.disponiblity.map((disponibility, index) => (
+                                    <DisponibilityComponent
+                                        key={index}
+                                        value={disponibility}
+                                        isSelected={disponibilityStatus[index]}
+                                        onClicked={() => {
+                                            selectDisponibility(index)
+                                        }}
+                                    />
+                                ))
+                            }
+                        </div>
+                    </div>
+
+                </form>
+
+            </div>
+
 
         </Modal>
     )
