0d445366c8524d319a262c69882dce36fd54a756
Micherlane Silva <micherlanerod@gmail.com>
2023-12-07 10:20:57 -0300

    .

--- GetDog/frontend/src/components/ImageUser/index.tsx
+++ GetDog/frontend/src/components/ImageUser/index.tsx
@@ -7,7 +7,7 @@ interface ImageUserProps {
 
 export function ImageUser({ urlImage, size } : ImageUserProps){
     const pathImage = urlImage !== null ? urlImage : 'user.png';
-    const urlPath = `http://localhost:3001/images/${pathImage}`;
+    const urlPath = `https://getdogapi.onrender.com/images/${pathImage}`;
     return (
         <div className={styles.userImageContainer} style={{ width: `${size}px`, height: `${size}px` }}>
             <img src={urlPath} />
